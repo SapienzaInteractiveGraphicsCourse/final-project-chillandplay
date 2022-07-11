c00c713243ea6f3ba3775435098de1d32ffe5173
Mario Angelini <mario@pedius.org>
2022-07-11 14:29:36 +0200

    total update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import * as THREE from './node_modules/three/build/three.module.js'
+import * as THREE from './libs/three/build/three.module.js'
 
 // -------------- VARIABLES DECLARATION ---------------------
 let flyBody, flyEyes, flyBiggerWings, flySmallerWings;
